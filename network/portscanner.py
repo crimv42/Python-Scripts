242109f0bef1a59139bf1321cdca2db1ea40c102
BatDan <crimvo@gmail.com>
2018-05-01 16:10:57 -0600

    Update portscanner.py

--- network/portscanner.py
+++ network/portscanner.py
@@ -2,16 +2,13 @@
 import socket
 import subprocess
 import sys
+import argparse
 from datetime import datetime
 from time import sleep
 
-# Clear the screen
-subprocess.call('clear', shell=True)
-
 # Ask for input
 def get_host():
-    remote_server = input("Enter a remote host to scan: ")
-    print (remote_server)
+    remote_server = input("Enter a remote hostname to scan: ")
     try:
         remote_server_ip = socket.gethostbyname(remote_server)
         return remote_server_ip
@@ -21,10 +18,8 @@ def get_host():
 
 # Print a nice banner with information on which host we are about to scan
 def banner(remote_server_ip):
-    message = ("-" * 60 + "\nPlease wait, scanning remote host: %s\n" % remote_server_ip + "-" * 60)
-    #print ("-" * 60)
-    #print ("Please wait, scanning remote host", remote_server_ip)
-    #print ("-" * 60)
+    message = ("-" * 60 + "\nPlease wait, scanning remote host: %s\n" 
+                % remote_server_ip + "-" * 60)
     return message
 
 def time_spent(x, time=datetime.now()):
@@ -42,19 +37,25 @@ def get_port_range():
     port_range = (start_port, (end_port + 1))
     return port_range
 
-def port_scan(remote_server_ip):
+def port_scan(remote_server_ip, full_scan=False):
     port_range = get_port_range()
+    print (banner(remote_server_ip))
+    
     try:
         print ("\n {0:1s}: {1:>9}:".format('Port', 'Status'))
         for port in range(port_range[0], port_range[1]):
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.settimeout(5)
             result = sock.connect_ex((remote_server_ip, port))
-            if result == 0:
-                print (" {0:< 5d} {1:>10}".format(port, 'Open'))
+            if full_scan == True:
+                if result == 0:
+                    print (" {0:< 5d} {1:>10}".format(port, 'Open'))
+                else:
+                    print (" {0:< 5d} {1:>10}".format(port, 'Closed'))
+                sock.close()
             else:
-                print (" {0:< 5d} {1:>10}".format(port, 'Closed'))
-            sock.close()
+                if result == 0:
+                    print (" {0:< 5d} {1:>10}".format(port, 'Open'))
         return ("\nScan Completed!\n")
     
     except KeyboardInterrupt:
@@ -69,12 +70,35 @@ def port_scan(remote_server_ip):
         print ("Couldn't connect to server!")
         sys.exit()
 
+## Parse any arguments
+parser = argparse.ArgumentParser(description="Set optional flags for PortScanner")
+parser.add_argument('-a', '--all', action='store_true', help="Display all ports. Closed, and Open")
+parser.add_argument('-i', '--ip', help="Provide direct IP of Host")
+args = parser.parse_args()
 
+# Clear the screen
+subprocess.call('clear', shell=True)
 
-remote_server_ip = get_host()
-print (banner(remote_server_ip))
+# Start the program
 
-time_1 = time_spent('start')
-print (port_scan(remote_server_ip))
+if args.all == True:
+    if args.ip != None:
+        remote_server_ip = args.ip
+        time_1 = time_spent('start')
+        print (port_scan(remote_server_ip, args.all))
+    else:
+        remote_server_ip = get_host()
+        time_1 = time_spent('start')
+        print (port_scan(remote_server_ip, args.all))
+else:
+    if args.ip != None:
+        remote_server_ip = args.ip
+        time_1 = time_spent('start')
+        print (port_scan(remote_server_ip))
+    else:
+        remote_server_ip = get_host()
+        time_1 = time_spent('start')
+        print (port_scan(remote_server_ip))
+
 
 print ("Time spent: ", time_spent('stop', time_1))
